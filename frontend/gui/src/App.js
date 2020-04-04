a74583f04d152a9f25d831787f4d48c31f70a3b4
=rxharja <=redon.xharja@gmail.com>
2020-04-04 02:10:25 +0100

    set up rest api, react frontend framework

---
+++ frontend/gui/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import 'antd/dist/antd.css'
+
+import CustomLayout from './containers/Layout'
+import ArticleList from './containers/ArticleListView'
+
+function App() {
+  return (
+    <div className="App">
+      <CustomLayout>
+        <ArticleList />
+      </CustomLayout>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/gui/src/containers/ArticleListView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Article from '../components/Article';
+
+
+class ArticleList extends React.Component {
+  render() {
+    return (
+      <Article />
+    );
+  }
+}
+
+export default ArticleList
