921e161cd9e19e4393b6e7933c5a930c559bed75
=rxharja <=redon.xharja@gmail.com>
2020-04-05 16:44:41 +0100

    added header that changes with pages

--- frontend/gui/src/containers/Layout.js
+++ frontend/gui/src/containers/Layout.js
@@ -1,43 +1,84 @@
 import React from 'react';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
 import { connect } from 'react-redux';
 import { Link, withRouter } from 'react-router-dom';
 import * as actions from '../store/actions/auth';
+import TopHeader from '../components/TopHeader'
 
-const { Header, Content, Footer } = Layout;
+const { Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
 
 class CustomLayout extends React.Component {
+  state = {
+    collapsed: false,
+  };
 
-  render(){
+  componentDidMount() {
+    console.log(this.props);
+  }
+
+  onCollapse = collapsed => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
+
+  render() {
     return (
-      <Layout className="layout">
-        <Header>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
           <div className="logo" />
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={['2']}
-            style={{ lineHeight: '64px' }}
-          >
-          {
-            this.props.isAuthenticated ?
-            <Menu.Item key="2" onClick={this.props.logout}><Link to="/logout">Logout</Link></Menu.Item>
-            :
-            <Menu.Item key="2"><Link to="/login">Login</Link></Menu.Item>
-          }
-            <Menu.Item key="1"><Link to="/">Posts</Link></Menu.Item>
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            {
+              this.props.isAuthenticated ?
+
+              <Menu.Item key="1" onClick={this.props.logout} >
+                <Icon type="logout" />
+                <span>Logout</span>
+              </Menu.Item>
+              :
+              <Menu.Item key="1">
+                <Icon type="login" />
+                <span><Link to="/login">Login</Link></span>
+              </Menu.Item>
+            }
+            <Menu.Item key="2">
+              <Icon type="home" />
+              <span><Link to="/">Home</Link></span>
+            </Menu.Item>
+            <SubMenu
+              key="sub1"
+              title={
+                <span>
+                  <Icon type="fire" />
+                  <span>About</span>
+                </span>
+              }
+            >
+              <Menu.Item key="3">Who I am</Menu.Item>
+              <Menu.Item key="4">Projects</Menu.Item>
+              <Menu.Item key="5">Resume</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="6">
+              <Icon type="smile" />
+              <span><Link to="/articles">Blog</Link></span>
+            </Menu.Item>
           </Menu>
-        </Header>
-        <Content style={{ padding: '0 50px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item><Link to="/">Home</Link></Breadcrumb.Item>
-            <Breadcrumb.Item><Link to="/">List</Link></Breadcrumb.Item>
-          </Breadcrumb>
-          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
-            {this.props.children}
-          </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Sider>
+        <Layout>
+
+          <Content style={{ margin: '0 16px' }}>
+            <TopHeader
+            location = {this.props.location}
+            history = {this.props.history}
+            style={{ background: '#fff', padding: 0 }} />
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>{this.props.username}</Breadcrumb.Item>
+            </Breadcrumb>
+            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>{this.props.children}</div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>
       </Layout>
     );
   }
@@ -50,3 +91,5 @@ const mapDispatchToProps = (dispatch, event) => {
 }
 
 export default withRouter(connect(null, mapDispatchToProps)(CustomLayout));
+
+//           <TopHeader style={{ background: '#fff', padding: 0 }} />
--- frontend/gui/src/routes.js
+++ frontend/gui/src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
+import Home from './containers/Home'
 import ArticleList from './containers/ArticleListView'
 import ArticleDetail from './containers/ArticleDetailView'
 import Login from './containers/Login';
@@ -8,7 +9,8 @@ import Signup from './containers/Signup';
 
 const BaseRouter = () => (
   <div>
-    <Route exact path='/' component={ArticleList} />{" "}
+    <Route exact path='/' component={Home} />{" "}
+    <Route exact path='/articles/' component={ArticleList} />{" "}
     <Route exact path='/articles/:articleID/' component={ArticleDetail} />{" "}
     <Route exact path='/login/' component={Login} />{" "}
     <Route exact path='/signup/' component={Signup} />{" "}
