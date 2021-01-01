a2ab2b618959377cf9ab7c0151487c36d7cbc84c
Redon Xharja <redon.xharja@gmail.com>
2021-01-01 15:20:40 -0500

    fixed how images are stored

--- backend/src/articles/models.py
+++ backend/src/articles/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 # Create your models here.
 
 
@@ -8,8 +7,8 @@ class Article(models.Model):
     title = models.CharField(max_length=120)
     created = models.DateTimeField(auto_now=True)
     description = models.CharField(max_length=250, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
     content = models.TextField()
+    image = models.ImageField(null=True, blank=True, upload_to='assets/')
 
     def __str__(self):
         return self.title
